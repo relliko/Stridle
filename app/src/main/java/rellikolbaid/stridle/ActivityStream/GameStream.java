b5841f93fda263f224d0f4833bd23b366d981fa6
Alex Polywka <apolywka@gmail.com>
2016-08-26 15:53:39 -0700

    haba daba dada dee ba bada bo we got a lotta coke

---
+++ app/src/main/java/rellikolbaid/stridle/ActivityStream/GameStream.java
@@ -0,0 +1,9 @@
+package rellikolbaid.stridle.activitystream;
+
+/**
+ * Created by Rellikolbaid on 8/25/2016.
+ */
+public class GameStream extends Thread{ // idk what im doing help
+
+
+}
--- app/src/main/java/rellikolbaid/stridle/MainActivity.java
+++ app/src/main/java/rellikolbaid/stridle/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         // TODO: load saved points
         pointsView.setText(message);
 
+
     }
 
     protected void onStart() {
