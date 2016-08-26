9226fc23c9aa7e3833b26fdedcab0f25409a295a
Alex Polywka <apolywka@gmail.com>
2016-08-25 22:48:02 -0700

    It runs, but does nothing

--- app/src/main/java/rellikolbaid/stridle/Display.java
+++ app/src/main/java/rellikolbaid/stridle/Display.java
@@ -1,13 +1,23 @@
 package rellikolbaid.stridle;
 
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by Rellikolbaid on 8/25/2016.
  */
-public class Display implements Runnable {
+public class Display extends AppCompatActivity implements Runnable {
+    int points = 0;
+
+    @Override
     public void run() {
-        System.out.println("Hello from a thread!");
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView)this.findViewById(R.id.points);
+        String message = Integer.toString(points);
+        while(true) {
+            textView.setText(message);
+        }
 
     }
 
-}
 }
--- app/src/main/java/rellikolbaid/stridle/MainActivity.java
+++ app/src/main/java/rellikolbaid/stridle/MainActivity.java
@@ -6,30 +6,14 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static int points = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        TextView textView = (TextView)this.findViewById(R.id.points);
+        String message = Integer.toString(points);
+        textView.setText(message);
+
     }
-
-    /**TextView textView = (TextView)this.findViewById(R.id.points);
-    Runnable myRunnable = new Runnable() {
-        @Override
-        public void run() {
-            while (true) {
-                int points = 0; // set initial points value. change to load saved val later
-                final String message = Integer.toString(points);
-                textView.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        textView.setText(message);
-                    }
-                });
-            }
-        }
-
-    }; */
-
 }
