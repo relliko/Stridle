94ffe895243a0f5cf7de5b11850c0f633129faed
Alex Polywka <apolywka@gmail.com>
2016-08-26 02:46:55 -0700

    It runs, but does nothing

--- app/src/main/java/rellikolbaid/stridle/MainActivity.java
+++ app/src/main/java/rellikolbaid/stridle/MainActivity.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView pointsView; // Member variable for viewing points in the layout
+    TextView pointsView; // Member variable for viewing points value in the layout
     static int points = 1234; // Defines the total number of points. TODO: replace with actual counted steps
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // Bundle has to do with save states.
         super.onCreate(savedInstanceState); // Superclass always needs to be called. idk why tho
 
         // Set the user interface layout for the main activity.
@@ -27,25 +27,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void onStart() {
+        /**
+         * Every time the activity becomes visible this is called.
+         */
         super.onStart();
     }
 
     protected void onResume() {
+        /**
+         * Called at any time that the activity is in the foreground.
+         */
         super.onResume();
     }
 
     protected void onPause() {
+        /**
+         * This is when the activity is still partially visible but the user may be leaving it
+         * like when they're opening a menu over it. Save anything that must be autosaved etc
+         */
         super.onPause();
     }
 
     protected void onStop() {
+        /**
+         * When this is called the activity is no longer visible and should release almost all
+         * resources that aren't needed while the user is not using it. Intensive shut-down
+         * operations should be performed here.
+         */
         super.onStop();
+    }
+
+    protected void onRestart() {
+        /**
+         * Called if the user returns while the activity is stopped. It is followed by onStart()
+         * and onResume().
+         */
     }
 
     protected void onDestroy() {
         super.onDestroy();
 
-        // This makes sure the app kills all threads created by the app when it is destroyed
+        // This makes sure the app kills all threads created by the app when it is destroyed.
+        // We need this since we're going to have background threading going.
         android.os.Debug.stopMethodTracing();
     }
 
---
+++ app/src/main/java/rellikolbaid/stridle/StepSensorFragment.java
@@ -0,0 +1,7 @@
+package rellikolbaid.stridle;
+
+/**
+ * Created by Rellikolbaid on 8/26/2016.
+ */
+public class StepSensorFragment {
+}
